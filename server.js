a9100962f67b71e6db7f68503d74f2b00868dce0
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-06-21 13:59:53 +0200

    Add base routes

--- server.js
+++ server.js
@@ -10,10 +10,22 @@ const URI = process.env.URI
 mongoose.connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+})
+.then(console.log('Database connected'))
+.catch((err) => {console.log(err)})
 
 const app = express()
 app.use(express.json())
+
+const indexRoute = require('./routes/index')
+const postRoute = require('./routes/post')
+const authRoute = require('./routes/auth')
+const userRoute = require('./routes/user')
+
+//app.use('/'. indexRoute)
+app.use('/post', postRoute)
+//app.use('/auth', authRoute)
+//app.use('/user', userRoute)
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
