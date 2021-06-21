2b499208c238af6dc64daf0eca764701edd324c8
61619760 </>
2021-06-21 12:48:52 +0200

    Connect to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+if (!process.env.MODE) {
+    require('dotenv').config()
+}
 const express = require('express')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
+const URI = process.env.URI
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const app = express()
 app.use(express.json())
